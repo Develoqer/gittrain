e7c430514a8262f72b9072cdca39d87ae2fdf012
Hamid KEZZAL <Hamidkezzal@free.fr>
2020-05-14 16:01:38 +0200

    bouton pour changer le background

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function modifyBackGroundColor(new_color) {
-    let button = document.getElementById("backcolor");
-    button.addEventListener("click", 
-    () => { 
-        modifyColor(""); }, false);    
+
+const button = document.getElementById("backcolor");
+// const corps = document.getElementsByTagName("body");
+// console.log("corps");
+
+
+button.addEventListener("click",colorBack);
+
+
+
+function colorBack() {
+     document.body.classList.toggle("background").add("background");
   }
